791c645d2a1f9aa47ca66b6462697f72842320ad
Justin Ryan <jryan@netflix.com>
2014-10-03 09:45:38 -0700

    Grab projects

--- src/main/java/halfempty/TestStashClient.java
+++ src/main/java/halfempty/TestStashClient.java
@@ -1,10 +1,13 @@
 package halfempty;
 
 import com.atlassian.stash.rest.client.api.StashClient;
+import com.atlassian.stash.rest.client.api.entity.Page;
+import com.atlassian.stash.rest.client.api.entity.Project;
 import com.atlassian.stash.rest.client.httpclient.HttpClientConfig;
 import com.atlassian.stash.rest.client.httpclient.HttpClientStashClientFactory;
 import com.atlassian.stash.rest.client.httpclient.HttpClientStashClientFactoryImpl;
 import java.net.URL;
+import java.util.List;
 import java.util.Map;
 
 public class TestStashClient {
@@ -21,6 +24,18 @@ public class TestStashClient {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
 
-        client
+        Page<Project> projectPage = client.getAccessibleProjects(0, 10);
+        do {
+            // Print it out
+            List<Project> projects = projectPage.getValues();
+            for(Project project: projects) {
+                System.out.println(project.getKey() + ":" + project.getName() + " - " + project.getDescription());
+            }
+
+            // Get next page
+            if (projectPage != null) {
+                projectPage = client.getAccessibleProjects(projectPage.getNextPageStart().longValue(), 10);
+            }
+        } while(projectPage != null && !projectPage.isLastPage());
     }
 }
